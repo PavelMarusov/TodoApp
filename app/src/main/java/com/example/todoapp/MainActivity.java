770701a93e322d723dae4d2fca099c6d4fe83ca2
PavelMarusov <pavelmarusovs@gmail.com>
2020-02-26 15:54:17 +0600

    add size activity

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.todoapp;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -8,6 +9,8 @@ import com.example.todoapp.ui.onBoard.OnBoardActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.os.Environment;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -26,20 +29,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private final int RC_WRITE_EXTERNAL = 101;
+    private File file;
+    private File foldet;
+    EditText editText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         boolean isShow = Prefs.getInstance(this).isShown();
-         if (!isShow){
-             startActivity(new Intent(this, OnBoardActivity.class));
-             finish();
-             return;
-         }
+
+
+        boolean isShow = Prefs.getInstance(this).isShown();
+        if (!isShow) {
+            startActivity(new Intent(this, OnBoardActivity.class));
+            finish();
+            return;
+        }
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -47,8 +68,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,FormActivity.class);
-                startActivityForResult(intent,100);
+                Intent intent = new Intent(MainActivity.this, FormActivity.class);
+                startActivityForResult(intent, 100);
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -63,6 +84,59 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+    }
+
+    @AfterPermissionGranted(RC_WRITE_EXTERNAL)
+    private void initFile(String content) {
+        String premision = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        if (EasyPermissions.hasPermissions(this, premision)) {
+            file = new File(Environment.getExternalStorageDirectory(), "TodoApp");
+            file.mkdir();
+            foldet = new File(file, "note.txt");
+            try {
+
+                foldet.createNewFile();
+                FileOutputStream fos = new FileOutputStream(foldet);
+                fos.write(content.getBytes());
+                fos.close();
+                Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            EasyPermissions.requestPermissions(this, "", RC_WRITE_EXTERNAL, premision);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        editText  = findViewById(R.id.edit_text);
+        initFile(editText.getText().toString());
+
+    }
+
+    public void save(String text){
+        File file = new File(Environment.getExternalStorageDirectory(),"note.txt");
+
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(text.getBytes());
+            fos.close();
+            Toast.makeText(this,"Saved",Toast.LENGTH_SHORT).show();
+        }catch (FileNotFoundException fnfE){
+            fnfE.printStackTrace();
+            Toast.makeText(this,"File not found",Toast.LENGTH_SHORT).show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 
     @Override
@@ -83,7 +157,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode==RESULT_OK&&requestCode==100){
+        if (resultCode == RESULT_OK && requestCode == 100) {
             String text = data.getStringExtra("title");
 
         }
@@ -91,10 +165,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_settings:
                 Prefs.getInstance(this).deleteAll();
-
+                finish();
+            case R.id.text_size:
+                Intent intent = new Intent(MainActivity.this, SizeActivity.class);
+                startActivity(intent);
 
         }
 
